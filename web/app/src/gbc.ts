ca4162f2981494f4c78d85b06899d07fc8cb58fc
Anne Castrillon <anne.castrillon@gmail.com>
2025-07-15 19:02:01 -0700

    another commit on web changes

--- web/app/src/gbc.ts
+++ web/app/src/gbc.ts
@@ -1,6 +1,34 @@
+import JSZip from 'jszip'
+
 export class GBC {
 
+
+  onGameChange(file: File) {
+    console.log(file)
+  }
+
   addEventListeners() {
+    const loadGame = document.getElementById("game-button")
+    const gameInput = document.getElementById("game-input")
+
+    if (loadGame != null && gameInput != null) {
+      gameInput.onchange = (ev) => {
+        const files = (ev.target as HTMLInputElement)?.files
+
+        if (files != null) {
+          const file = files[0]
+
+          this.onGameChange(file)
+        }
+      }
+
+      loadGame.onclick = (ev) => {
+        if (gameInput != null) {
+          gameInput.click()
+        }
+      }
+    }
+
     document.onkeydown = (ev) => {
       switch (ev.key) {
         case "Escape":
