68988424ae5284879e23931b8b36d29f761d5b11
Anne Castrillon <anne.castrillon@gmail.com>
2025-07-24 20:48:42 -0700

    minor changes again

--- desktop/src/frontend.rs
+++ desktop/src/frontend.rs
@@ -274,8 +274,11 @@ impl Frontend {
             .build()
             .unwrap();
 
+        let window_positon = window.position();
+
         let mut waveform_window = video_subsystem
             .window("Waveform Viewer", 683, 256)
+            .position((window_positon.0 as f32 * 1.60) as i32, window_positon.1)
             .build()
             .unwrap();
 
